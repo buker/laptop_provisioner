91a69694615deef5cfd1b84942d538d2ce66ec81
Łukasz Żułnowski <lzulnowski@gmail.com>
2018-10-19 17:15:52 +0200

    add readmy

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 To bootstrap you system you need to execute command on fresh system 
 
-wget https://raw.githubusercontent.com/buker/laptop_provisioner/master/bootstrap.sh | sudo bash
+wget -O - https://raw.githubusercontent.com/buker/laptop_provisioner/master/bootstrap.sh | sudo bash
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 user=`who|cut -d' ' -f1`
 echo $user
-sudo apt-get update
-sudo apt-get install ansible git 
+sudo apt-get update -yqq
+sudo apt-get install -yqq ansible git 
 mkdir -p /home/$user/Workspace
 cd /home/$user/Workspace 
 git clone https://github.com/buker/laptop_provisioner.git

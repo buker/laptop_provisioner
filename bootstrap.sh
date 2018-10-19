e5b7bc377ab0bf572d0a463a725ba6251a4f2e72
Łukasz Żułnowski <lzulnowski@gmail.com>
2018-10-19 18:07:15 +0200

    add ifs

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,15 @@ user=`who|cut -d' ' -f1`
 echo $user
 sudo apt-get update -yqq
 sudo apt-get install -yqq ansible git 
-if [ ! -d "/home/$user/Workspace" ]; then
+if [ ! -d "/home/$user/Workspace/laptop_provisioner" ]; then
     sudo -u $user mkdir -p /home/$user/Workspace
     cd /home/$user/Workspace 
     sudo -u $user git clone https://github.com/buker/laptop_provisioner.git
     cd laptop_provisioner
-    sudo ansible-playbook -K playbook.yml
+else 
+    cd /home/$user/Workspace/laptop_provisioner
+    git fetch
+    git checkout master
+    git pull
 fi 
+sudo ansible-playbook -K playbook.yml

5e8013f9a65aa1bbdbad655232b056102d178da8
Łukasz Żułnowski <lzulnowski@gmail.com>
2018-10-19 18:00:50 +0200

    add base ansible playbook

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,9 @@ user=`who|cut -d' ' -f1`
 echo $user
 sudo apt-get update -yqq
 sudo apt-get install -yqq ansible git 
-sudo -u $user mkdir -p /home/$user/Workspace
-cd /home/$user/Workspace 
-sudo -u $user git clone https://github.com/buker/laptop_provisioner.git
-cd laptop_provisioner
-
+if [ ! -d "/home/$user/Workspace" ]; then
+    sudo -u $user mkdir -p /home/$user/Workspace
+    cd /home/$user/Workspace 
+    sudo -u $user git clone https://github.com/buker/laptop_provisioner.git
+    cd laptop_provisioner
+fi 
